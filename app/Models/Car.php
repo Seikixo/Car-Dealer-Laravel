59ffae137d6a21f9c3c18f56fb7947ea5093c315
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-12-30 14:46:07 +0800

    filter car scope query function

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -33,4 +33,36 @@ class Car extends Model
         return $query->where('id', $id)->delete();
     }
 
+    public function scopeFilterCars($query, $filters){
+        if(isset($filters['make']) && !empty($filters['make'])){
+            $query->where('make', $filters['make']);
+        }
+
+        if(isset($filters['model']) && !empty($filters['model'])){
+            $query->where('model', $filters['model']);
+        }
+
+        if(isset($filters['color']) && !empty($filters['color'])){
+            $query->where('color', $filters['color']);
+        }
+
+        if(isset($filters['year']) && !empty($filters['year'])){
+            $query->where('year', $filters['year']);
+        }
+
+        if(isset($filters['price_min']) && !empty($filters['price_min'])){
+            $query->where('price', '>=', $filters['price_min']);
+        }
+
+        if(isset($filters['price_max']) && !empty($filters['price_max'])){
+            $query->where('price', '<=', $filters['price_max']);
+        }
+
+        if(isset($filters['status']) && !empty($filters['status'])){
+            $query->where('status', $filters['status']);
+        }
+
+        return $query->get();
+    }
+
 }
